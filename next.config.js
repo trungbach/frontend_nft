a2f5d4548578f6f49522990c9983e8409ac977bc
trungbach <trunga2k29@gmail.com>
2023-02-19 12:00:56 +0700

    update link backend

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
       domains: ['opensea.io', "storage.googleapis.com", "lh3.googleusercontent.com", "img.favpng.com", 
                 "www.designyourway.net", "thedreamwithinpictures.com", 'ipfs.infura.io', 
                 'payload.cargocollective.com', 'upload.wikimedia.org',
-                '139.177.188.72', "95.217.21.156", "localhost"
+                '139.177.188.72', "95.217.21.156", "localhost", "103.7.41.183"
               ],
     },
     trailingSlash: true,
