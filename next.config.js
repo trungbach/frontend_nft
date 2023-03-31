20cee55b9bb3eba9502a475eed0182ab474101ed
trungbach <trunga2k29@gmail.com>
2023-03-31 14:03:47 +0700

    refactor code

--- next.config.js
+++ next.config.js
@@ -1,22 +1,30 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-    reactStrictMode: true,
-    images: {
-      domains: ['opensea.io', "storage.googleapis.com", "lh3.googleusercontent.com", "img.favpng.com", 
-                "www.designyourway.net", "thedreamwithinpictures.com", 'ipfs.infura.io', 
-                'payload.cargocollective.com', 'upload.wikimedia.org',
-                '139.177.188.72', "95.217.21.156", "localhost", "103.7.41.183"
-              ],
-    },
-    trailingSlash: true,
-    react: {
-      useSuspense: false,
-      wait: true,
-    },
-
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "opensea.io",
+      "storage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "img.favpng.com",
+      "www.designyourway.net",
+      "thedreamwithinpictures.com",
+      "ipfs.infura.io",
+      "payload.cargocollective.com",
+      "upload.wikimedia.org",
+      "139.177.188.72",
+      "95.217.21.156",
+      "localhost",
+      "103.7.40.140",
+    ],
+  },
+  trailingSlash: true,
+  react: {
+    useSuspense: false,
+    wait: true,
+  },
+};
